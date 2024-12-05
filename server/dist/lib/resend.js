452457300818eeaacc2315f5c128d2cc66c02f93
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-12-05 22:57:22 +0400

    dist folder removed

--- server/dist/lib/resend.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resend_1 = require("resend");
-const env_1 = require("../constants/env");
-const resend = new resend_1.Resend(env_1.resend_api_key);
-exports.default = resend;
