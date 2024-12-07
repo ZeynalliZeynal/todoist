1bf360475763429be2be965b4deef2940c720d4e
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-12-07 18:51:43 +0400

    tooltips removed

--- app/(landing)/_layout/header/nav-right.tsx
+++ app/(landing)/_layout/header/nav-right.tsx
@@ -1,35 +1,19 @@
 "use client";
 
 import Button from "@/components/ui/button";
-import Tooltip from "@/components/ui/tooltip";
 
 export default function NavRight() {
   return (
     <div className="flex items-center gap-3">
-      <Tooltip openDelay={200} closeDelay={200}>
-        <Tooltip.Trigger>
-          <Button primary size="sm" href="/auth/login">
-            Log In
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Log in</Tooltip.Content>
-      </Tooltip>
-      <Tooltip openDelay={200} closeDelay={200}>
-        <Tooltip.Trigger>
-          <Button primary size="sm" href="/contact">
-            Contact
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content>Contact</Tooltip.Content>
-      </Tooltip>
-      <Tooltip openDelay={200} closeDelay={200}>
-        <Tooltip.Trigger>
-          <Button primary size="sm" href="/auth/signup">
-            Sign up
-          </Button>
-        </Tooltip.Trigger>
-        <Tooltip.Content align="vertical-right-center">Sign up</Tooltip.Content>
-      </Tooltip>
+      <Button primary size="sm" href="/auth/login">
+        Log In
+      </Button>
+      <Button primary size="sm" href="/contact">
+        Contact
+      </Button>
+      <Button primary size="sm" href="/auth/signup">
+        Sign up
+      </Button>
     </div>
   );
 }
