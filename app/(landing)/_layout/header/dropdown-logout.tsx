72d5428228a4064b12c06aab47526fb5e3edcd51
ZeynalliZeynal <zzeynalli446@gmail.com>
2025-01-01 03:31:56 +0400

    dropdown logout fixed

--- app/(landing)/_layout/header/dropdown-logout.tsx
+++ app/(landing)/_layout/header/dropdown-logout.tsx
@@ -1,17 +1,16 @@
-"use client";
+'use client';
 
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu/dropdown-menu";
-import { LuLogOut } from "react-icons/lu";
-import { useTransition } from "react";
-import Spinner from "@/components/ui/spinner";
-import { logout } from "@/actions/auth.actions";
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu/dropdown-menu';
+import { LuLogOut } from 'react-icons/lu';
+import { useTransition } from 'react';
+import Spinner from '@/components/ui/spinner';
+import { logout } from '@/actions/auth.actions';
 
 export default function DropdownLogout() {
   const [isPending, startTransition] = useTransition();
 
   return (
     <DropdownMenuItem
-      suffix={isPending ? <Spinner /> : <LuLogOut />}
       disabled={isPending}
       onClick={() =>
         startTransition(async () => {
@@ -20,6 +19,7 @@ export default function DropdownLogout() {
       }
     >
       Log out
+      <span className="ml-auto">{isPending ? <Spinner /> : <LuLogOut />}</span>
     </DropdownMenuItem>
   );
 }
