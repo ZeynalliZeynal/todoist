4ce300f5802ca5b3598202b7e897487b657a2f25
ZeynalliZeynal <zzeynalli446@gmail.com>
2025-01-10 20:03:43 +0400

    feat(geist): setup checkbox page

---
+++ app/(geist)/geist/checkbox/page.tsx
@@ -0,0 +1,12 @@
+export default function CheckboxPage() {
+  return (
+    <div className="divide-y">
+      <section className="p-12">
+        <h1 className="mb-3">Checkbox</h1>
+        <p className="text-gray-900">
+          A control that toggles between two options, checked or unchecked.
+        </p>
+      </section>
+    </div>
+  );
+}
