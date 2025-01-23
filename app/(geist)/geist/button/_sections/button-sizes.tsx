93d135d61f51213d8fb043f83c2fbf5fe2670d2b
Zeynalli Zeynal <zzeynalli446@gmail.com>
2025-01-24 01:16:02 +0400

    lib: add button from library to test it

--- app/(geist)/geist/button/_sections/button-sizes.tsx
+++ app/(geist)/geist/button/_sections/button-sizes.tsx
@@ -4,7 +4,7 @@ import {
   CollapseContent,
   CollapseTrigger,
 } from '@/components/ui/collapse';
-import { Button } from '@/components/ui/button';
+import { Button } from '@everest-ui/react-button';
 import CodeBlock from '@/components/ui/code-block';
 import { BUTTON_SIZES_CODE } from '@/app/(geist)/geist/button/_snippets/snippets';
 import GeistContentWrapper from '@/app/(geist)/geist/_components/geist-content-wrapper';
