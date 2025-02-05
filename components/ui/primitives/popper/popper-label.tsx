4be79a906897f05d43a461f0e015fd12573a5a28
ZeynalliZeynal <zzeynalli446@gmail.com>
2025-02-05 13:38:26 +0400

    fix: remove popper files

--- components/ui/primitives/popper/popper-label.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const PopperLabel = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentProps<"div">
->((props, ref) => {
-  return <div ref={ref} {...props} />;
-});
-PopperLabel.displayName = "PopperLabel";
